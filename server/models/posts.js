cb11eb30fa0c0772b4465a6b882e88518c6c450a
yogggy <102720855+yogggy@users.noreply.github.com>
2022-08-09 17:09:54 +0530

    Delete server directory

--- server/models/posts.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    username:{type:String},
-    caption:{type:String},
-    img:{type:String}, 
-});
-const PostData = new mongoose.model('posts',postSchema);
-module.exports = PostData;
